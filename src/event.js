dfd44ef74f3a1d2df7ac0df7c4a96f379027cf9e
SoursopID <soursopid@gmail.com>
2025-02-18 15:06:32 +0800

    compacting lines

--- src/event.js
+++ src/event.js
@@ -13,35 +13,22 @@
 export const CONNECTION_UPDATE = "connection.update";
 export const CREDS_UPDATE = "creds.update";
 export const MESSAGING_HISTORY_SET = "messaging-history.set";
-export const CONNECTION_EVENTS = [
-  CONNECTION_UPDATE,
-  CREDS_UPDATE,
-  MESSAGING_HISTORY_SET
-];
+export const CONNECTION_EVENTS = [CONNECTION_UPDATE, CREDS_UPDATE, MESSAGING_HISTORY_SET];
 
 // Chat events
 export const CHATS_UPSERT = "chats.upsert";
 export const CHATS_UPDATE = "chats.update";
 export const CHATS_DELETE = "chats.delete";
-export const CHAT_EVENTS = [
-  CHATS_UPSERT,
-  CHATS_UPDATE,
-  CHATS_DELETE
-];
+export const CHAT_EVENTS = [CHATS_UPSERT, CHATS_UPDATE, CHATS_DELETE];
 
 // Presence events
 export const PRESENCE_UPDATE = "presence.update";
-export const PRESENCE_EVENTS = [
-  PRESENCE_UPDATE
-];
+export const PRESENCE_EVENTS = [PRESENCE_UPDATE];
 
 // Contact events
 export const CONTACTS_UPSERT = "contacts.upsert";
 export const CONTACTS_UPDATE = "contacts.update";
-export const CONTACT_EVENTS = [
-  CONTACTS_UPSERT,
-  CONTACTS_UPDATE
-];
+export const CONTACT_EVENTS = [CONTACTS_UPSERT, CONTACTS_UPDATE];
 
 // Message events
 export const MESSAGES_DELETE = "messages.delete";
@@ -49,29 +36,17 @@ export const MESSAGES_UPDATE = "messages.update";
 export const MESSAGES_UPSERT = "messages.upsert";
 export const MESSAGES_REACTION = "messages.reaction";
 export const MESSAGE_RECEIPT_UPDATE = "message-receipt.update";
-export const MESSAGE_EVENTS = [
-  MESSAGES_DELETE,
-  MESSAGES_UPDATE,
-  MESSAGES_UPSERT,
-  MESSAGES_REACTION,
-  MESSAGE_RECEIPT_UPDATE
-];
+export const MESSAGE_EVENTS = [MESSAGES_DELETE, MESSAGES_UPDATE, MESSAGES_UPSERT, MESSAGES_REACTION, MESSAGE_RECEIPT_UPDATE];
 
 // Group events
 export const GROUPS_UPSERT = "groups.upsert";
 export const GROUP_PARTICIPANTS_UPDATE = "group-participants.update";
-export const GROUP_EVENTS = [
-  GROUPS_UPSERT,
-  GROUP_PARTICIPANTS_UPDATE
-];
+export const GROUP_EVENTS = [GROUPS_UPSERT, GROUP_PARTICIPANTS_UPDATE];
 
 // Blocklist events
 export const BLOCKLIST_SET = "blocklist.set";
 export const BLOCKLIST_UPDATE = "blocklist.update";
-export const BLOCKLIST_EVENTS = [
-  BLOCKLIST_SET,
-  BLOCKLIST_UPDATE
-];
+export const BLOCKLIST_EVENTS = [BLOCKLIST_SET, BLOCKLIST_UPDATE];
 
 // All events combined 
 export const ALL_EVENTS = [
