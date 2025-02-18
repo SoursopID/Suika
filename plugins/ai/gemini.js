d7864d6503b89bb21a9a274a00c707642578be44
SoursopID <soursopid@gmail.com>
2025-02-19 00:10:05 +0800

    add gemini ai

---
+++ plugins/ai/gemini.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2025 SoursopID
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Suika project
+ * (https://github.com/SoursopID/Suika)
+ */
+
+import { AllowOne, Plugin } from "../../dist/Plugin.js";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
+const genAI = new GoogleGenerativeAI(GEMINI_API_KEY);
+const MODEL_NAME = 'gemini-2.0-flash';
+const model = genAI.getGenerativeModel({ model: MODEL_NAME });
+const chat = model.startChat({});
+
+export let replyIDs = new Map();
+
+export const geminiAsk = async (m, query) => {
+  if (query?.length > 0) {
+    const resp = await chat.sendMessage(query)
+    if (resp?.response?.text) {
+      const re = await m.reply({ text: resp.response.text() });
+      if (re) replyIDs.set(re.key?.id, m.chat);
+      console.log(re, replyIDs);
+    } else {
+      console.log(resp);
+    }
+  }
+
+}
+
+export default new Plugin({
+  name: "gemini",
+  cmds: ["gm"],
+  checkRule: AllowOne,
+  checks: [
+    (m) => { return m.fromMe },
+  ],
+  exec: async (m) => {
+    let query = m.args;
+    if (m.quotedText?.length > 0) query = m.quotedText;
+    await geminiAsk(m, query);
+  }
+})
---
+++ plugins/ai/gemini_listen.js
@@ -0,0 +1,27 @@
+/**
+ * Copyright (C) 2025 SoursopID
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Suika project
+ * (https://github.com/SoursopID/Suika)
+ */
+
+import { MustAll, Plugin } from "../../dist/Plugin.js";
+import { geminiAsk, replyIDs } from "./gemini.js";
+
+export default new Plugin({
+  tags: ["gemini"],
+  checkRule: MustAll,
+  checks: [
+    (m) => { return replyIDs.has(m.stanzaId) },
+  ],
+  exec: async (m) => {
+    let query = m.text;
+
+    console.log(query)
+    await geminiAsk(m, query);
+  }
+})
