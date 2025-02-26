728b63646f503058e9aba627f314fa2f2e205738
SoursopID <soursopid@gmail.com>
2025-02-26 16:44:11 +0800

    add: config class

---
+++ src/config.js
@@ -0,0 +1,96 @@
+/**
+ * Copyright (C) 2025 SoursopID
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Suika project
+ * (https://github.com/SoursopID/Suika)
+ */
+
+import fs from 'fs';
+
+/**
+ * @typedef {Object} Config
+ * @property {string} [jsonName]
+ * @property {Map<string, any>} [data]
+ */
+
+/**
+ * @class
+ */
+export class Config {
+  /**
+   * 
+   * @param {Object} options 
+   * @param {string} options.jsonName
+   * @param {string} options.unique
+   */
+  constructor(options) {
+    /** @type {string} */
+    this.jsonName = options?.jsonName ?? `json_${options?.unique}.json`;
+
+    /** @type {Map<string, any>} */
+    this.data = new Map();
+
+    this.load(this.jsonName);
+  }
+
+  /** @param {string} filename */
+  async load(filename) {
+    if (!filename) filename = this.jsonName;
+    try {
+      const data = JSON.parse(fs.readFileSync(filename, 'utf8'));
+      for (const [key, value] of Object.entries(data)) {
+        this.data.set(key, value);
+      }
+
+      return true;
+    } catch (e) {
+      console.log(e)
+      return false;
+    }
+  }
+
+  /** @param {string} filename */
+  async save(filename) {
+    if (!filename) filename = this.jsonName;
+    try {
+      fs.writeFileSync(filename, JSON.stringify(Object.fromEntries(this.data), null, 2));
+      return true;
+    } catch (e) {
+      console.log(e);
+      return false;
+    }
+
+  }
+
+  /** @param {string} key */
+  get(key) {
+    return this.data.get(key);
+  }
+
+  /** 
+   * @param {string} key 
+   * @param {any} value 
+   */
+  set(key, value) {
+    this.data.set(key, value);
+  }
+
+  /** @param {string} key */
+  delete(key) {
+    this.data.delete(key);
+  }
+
+  /** @param {string} key */
+  has(key) {
+    return this.data.has(key);
+  }
+
+  clear() {
+    this.data.clear();
+  }
+
+}
