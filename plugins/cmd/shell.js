bc3cef522c723343090a9ada34c9d07e1990e0e7
SoursopID <soursopid@gmail.com>
2025-02-28 09:54:13 +0800

    add: shell cmd

---
+++ plugins/cmd/shell.js
@@ -0,0 +1,43 @@
+/**
+ * Copyright (C) 2025 SoursopID
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/
+ *
+ * This code is part of Suika project
+ * (https://github.com/SoursopID/Suika)
+ */
+
+import { exec } from "child_process";
+
+/** @type {import('../../src/plugin.js').Plugin} */
+export const on = {
+  cmds: ["#"],
+  timeout: 15,
+  noprefix: true,
+  checks: [
+    (m) => { return m.fromMe },
+  ],
+
+  /** @param {import('../../src/ctx.js').Ctx} [m] - context object */
+  exec: async (m) => {
+    if (m.args.length === 0) return;
+    if (m.includes('rm') && m.includes('-rf')) return;
+
+    // Use promisify to convert exec to promise-based
+    const util = require('util');
+    const execPromise = util.promisify(exec);
+
+    try {
+      const { stdout, stderr } = await execPromise(m.args);
+      if (stderr) {
+        console.log(stderr);
+        return;
+      }
+      await m.reply({ text: String(stdout), quote: m.msg });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
